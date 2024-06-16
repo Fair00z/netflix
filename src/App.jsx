99bc42c59d9826b2a5d54f7e4d17b7c0b1a6ecaa
Fairooz <154660523+Fair00z@users.noreply.github.com>
2024-06-16 12:29:14 +0000

    added more genres trending,comedy,romance,horror

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import NavBar from './Components/NavBar/NavBar'
 import RowPost from './Components/RowPost/RowPost'
 import Banner from './Components/banner/Banner'
 // import {orginals,action} from 'src/Constants/urls.jsx'
-import { orginals,action } from './Constants/urls'
+import { orginals,action,romance,comedy,horror,trending } from './Constants/urls'
 export default function App() {
   return (
     <div className='App'>
       <NavBar/>
       <Banner/>
       <RowPost url={orginals} title={'Netflix Originals'} class={'poster'}/>
+      <RowPost url={trending} title={'Trendings'} class={'poster-small'}/>
       <RowPost url={action} title={'Action'} class={'poster-small'}/>
+      <RowPost url={comedy} title={'Comedy'} class={'poster-small'}/>
+      <RowPost url={romance} title={'Romance'} class={'poster-small'}/>
+      <RowPost url={horror} title={'Horror'} class={'poster-small'}/>
     </div>
   )
 }
